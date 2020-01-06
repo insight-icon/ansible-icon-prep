627efd63d267804b0c038bb6d48af99c417dab05
robc-io <robcannonxyz@gmail.com>
2020-01-05 23:46:39 -0800

    adding additional states for waiting on sync completion

--- roles/wait-for-sync/files/wait-for-sync.sh
+++ roles/wait-for-sync/files/wait-for-sync.sh
@@ -4,8 +4,19 @@ until [[ $(curl -s -o /dev/null -w "%{http_code}" localhost:9000/api/v1/status/p
 	printf 'Syncing DB\n'
     sleep 5
 done
-until [[ $(curl -s localhost:9000/api/v1/status/peer | jq -r .state) == "Watch" ]]; do
-    printf 'Syncing Blocks\n'
-    sleep 5
+
+echo 'service is available and about to sync'
+
+declare -A array
+for stateCode in Watch Vote EvaluateNetwork
+do
+    array[$stateCode]=1
 done
 
+until [[ ${array[$(curl -s localhost:9000/api/v1/status/peer | jq -r .state)]} ]]; do
+    printf 'Syncing Blocks\n'
+    block_height=$(curl -s localhost:9000/api/v1/status/peer | jq -r .block_height)
+    echo Blockheight = $block_height
+    printf '\n'
+    sleep 5
+done
